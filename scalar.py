e1b40f6e851ee41f91191f4beda600464b4fe233
KenCorp <kennethwarui35@gmail.com>
2023-12-16 11:59:25 +0300

    created dir structure and initialized it

--- scalar.py
+++
@@ -1,9 +0,0 @@
-# Scalar types  such as String, Int, Float, Boolean and ID
-import graphene
-
-class Person(graphene.ObjectType):
-    name = graphene.String()
-    age = graphene.Int()
-    height = graphene.Float()
-    is_student = graphene.Boolean()
-    id = graphene.ID()
